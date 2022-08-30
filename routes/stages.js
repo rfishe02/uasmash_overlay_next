5fcc1708fa4c09768833238ce65c2c8a8bd708b2
rfishe02 <fisher.renae@gmail.com>
2022-08-30 05:27:59 -0500

    Separated css from the files.

--- routes/stages.js
+++ routes/stages.js
@@ -4,16 +4,14 @@ var router = express.Router();
 const db = require('../bin/db')
 
 router.get('/', async function(req, res, next) {
-
   try {
-
+    
     const result = await db.pool.query("SELECT * FROM stages");
     res.json(result);
 
   } catch (err) {
     next(err)
   }
-
 });
 
 module.exports = router;
