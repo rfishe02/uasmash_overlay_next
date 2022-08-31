a99364c0c0c176dc2707c77ec65f323809987474
rfishe02 <fisher.renae@gmail.com>
2022-08-30 20:24:10 -0500

    Created stage card component

--- public/javascripts/StageCard.js
+++ public/javascripts/StageCard.js
@@ -4,12 +4,12 @@ export default {
     url: ''
   },
   template: `
-  <div class="card text-center text-white border-secondary rounded shadow" style="background-color: rgba(0,0,0,1)">
+  <div class="card text-center text-white border-secondary rounded shadow" style="background-color: rgba(0,0,0,0.9);">
     <img class="rounded" v-bind:src="url" v-bind:alt="item.stage_name" style="opacity: 0.9"/>
     <div class="card-img-overlay">
-      <p style="font-size: 12pt">{{ item.stage_name }}</p>
-      <i v-if="item.choice == 'PICK'" class="bi bi-check-circle" style="font-size: 16pt"></i>
-      <i v-if="item.choice == 'STRIKE'" class="bi bi-x-circle-fill" style="font-size: 16pt"></i>
+      <p style="font-size: 12pt; font-weight: bold;">{{ item.stage_name }}</p>
+      <i v-if="item.choice == 'PICK'" class="bi bi-check-circle" style="font-size: 18pt"></i>
+      <i v-if="item.choice == 'STRIKE'" class="bi bi-x-circle-fill" style="font-size: 18pt"></i>
     </div>
   </div>
   `
