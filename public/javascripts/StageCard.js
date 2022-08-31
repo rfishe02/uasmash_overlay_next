0976011ac1ba3f0a348012ff44cbc993d2a53516
rfishe02 <fisher.renae@gmail.com>
2022-08-31 01:26:29 -0500

    Improve look and behavior of stage striking app. Cards stay in the same row, and they're not all cleared after picking stages.

--- public/javascripts/StageCard.js
+++ public/javascripts/StageCard.js
@@ -5,7 +5,7 @@ export default {
   },
   template: `
   <div class="card text-center text-white border-secondary rounded shadow" style="background-color: rgba(0,0,0,0.9);">
-    <img class="rounded" v-bind:src="url" v-bind:alt="item.stage_name" style="opacity: 0.9"/>
+    <img class="rounded card-img" v-bind:src="url" v-bind:alt="item.stage_name" style="opacity: 0.9"/>
     <div class="card-img-overlay">
       <p style="font-size: 12pt; font-weight: bold;">{{ item.stage_name }}</p>
       <i v-if="item.choice == 'PICK'" class="bi bi-check-circle" style="font-size: 18pt"></i>
