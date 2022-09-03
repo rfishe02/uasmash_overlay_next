a001433da6c12b57ed52a53741ff9bf44d8206aa
rfishe02 <fisher.renae@gmail.com>
2022-09-03 13:12:40 -0500

    Style fixes

--- public/javascripts/StageCardLarge.js
+++ public/javascripts/StageCardLarge.js
@@ -4,7 +4,7 @@ export default {
     url: '',
   },
   template: `
-  <div class="card text-center text-white rounded shadow" style="background-color: rgba(0,0,0,1); border: 0;">
+  <div class="card text-center text-white rounded shadow" style="background-color: rgba(0,0,0,1); border-width: 3px; border-color: rgba(255,255,255,1);">
     <img class="rounded card-img" v-bind:src="url" v-bind:alt="item.stageName" style="opacity: 0.8"/>
     <div class="card-img-overlay">
       <i v-if="item.choice == 'PICK'" class="bi bi-check-circle" style="font-size: 24pt;"></i>
