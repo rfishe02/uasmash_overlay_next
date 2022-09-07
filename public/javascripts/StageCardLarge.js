d080e8cac80fc442c9cf32c4fdf0242cf8549596
rfishe02 <fisher.renae@gmail.com>
2022-09-06 19:48:21 -0500

    Modified styling of the stage strike overlay and application.

--- public/javascripts/StageCardLarge.js
+++ public/javascripts/StageCardLarge.js
@@ -7,9 +7,9 @@ export default {
   <div class="card text-center text-white rounded shadow" style="background-color: rgba(0,0,0,1); border-width: 3px; border-color: rgba(255,255,255,1);">
     <img class="rounded card-img" v-bind:src="url" v-bind:alt="item.stageName" style="opacity: 0.8"/>
     <div class="card-img-overlay">
-      <i v-if="item.choice == 'PICK'" class="bi bi-check-circle" style="font-size: 24pt;"></i>
-      <i v-if="item.choice == 'STRIKE'" class="bi bi-x-circle-fill" style="font-size: 24pt;"></i>
-      <p style="font-size: 14pt; font-weight: bold;">{{ item.stageName }}</p>
+      <i v-if="item.choice == 'PICK'" class="bi bi-check-circle-fill" style="font-size: 24pt;"></i>
+      <i v-if="item.choice == 'STRIKE'" class="bi bi-x-circle" style="font-size: 24pt;"></i>
+      <p style="font-size: 18pt; font-weight: bold; padding-bottom: 15px;">{{ item.stageName }}</p>
     </div>
   </div>
   `
--- public/javascripts/StageCardSmall.js
+++ public/javascripts/StageCardSmall.js
@@ -7,9 +7,9 @@ export default {
   <div class="card text-center text-white border-secondary rounded shadow" style="background-color: rgba(0,0,0,1);">
     <img class="rounded card-img" v-bind:src="url" v-bind:alt="item.stageName" style="opacity: 0.8"/>
     <div class="card-img-overlay">
-      <i v-if="item.choice == 'PICK'" class="bi bi-check-circle" style="font-size: 18pt;"></i>
-      <i v-if="item.choice == 'STRIKE'" class="bi bi-x-circle-fill" style="font-size: 18pt;"></i>
-      <p style="font-size: 12pt; font-weight: bold;">{{ item.stageName }}</p>
+      <i v-if="item.choice == 'PICK'" class="bi bi-check-circle-fill" style="font-size: 16pt;"></i>
+      <i v-if="item.choice == 'STRIKE'" class="bi bi-x-circle" style="font-size: 16pt;"></i>
+      <p style="font-size: 12pt;">{{ item.stageName }}</p>
     </div>
   </div>
   `
