d2c64a438b8c108b86a91b2d91143c79ed7e5511
rfishe02 <fisher.renae@gmail.com>
2022-09-09 00:40:19 -0500

    Removed apiKey from url

--- routes/overlay.js
+++ routes/overlay.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/overlay', function(req, res, next) {
-  res.render('overlay', {  });
+  res.render('overlay', { apiKey: process.env.SMASHGG_KEY });
 });
 
 module.exports = router;
